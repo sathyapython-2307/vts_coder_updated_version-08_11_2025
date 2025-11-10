c15ee586a90b59c650c76e5a6e8c9b43a31c0c88
Sathya-735 <sathyasiva735@gmail.com>
2025-11-10 14:28:20 +0530

    Fix build configuration and package versions

--- core/settings.py
+++ core/settings.py
@@ -80,7 +80,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core.middleware.SecurityHeadersMiddleware',
 ]
 
 # Security settings
